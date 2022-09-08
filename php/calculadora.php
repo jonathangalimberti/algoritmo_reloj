d05626da35f6e490af9d59d692b4265c9e595037
jona-11 <jonathangalimberti@hotmail.com>
2022-09-08 15:01:19 -0300

    se agrega calculadora en version PHP

---
+++ php/calculadora.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    
+  <?php $valor1= 8;
+          $valor2= 3;
+
+$resultado = $valor1 + $valor2;
+print($resultado);
+?> 
+
+    <script src="https://replit.com/public/js/replit-badge.js" theme="blue" defer></script> 
+  </body>
+</html>
