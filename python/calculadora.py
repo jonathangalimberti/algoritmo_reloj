c83e98087f2f43a22d6f3f6396e41793b87449b7
jona-11 <jonathangalimberti@hotmail.com>
2022-09-08 10:26:02 -0300

    operaciones basicas con python

---
+++ python/calculadora.py
@@ -0,0 +1,7 @@
+#crear calculadora con operaciones matematicas basicas
+
+valor1 =7
+valor2 =4
+resultado = valor1 + valor2
+
+print(resultado)
--- reloj/reloj.js
+++ reloj/reloj.js
@@ -6,28 +6,27 @@ let m = 00;
 let s = 00;
 
 
-function time(h,m,s){
-setInterval(() => {
-    
-         if(s<60){
-        
-        d.innerHTML=`tiempo transcurrido ${h}:${m}:${s}`;
-            s=s+1;
-           
-        } 
-        if (s === 60){
-            s=00; 
-            m=m+1;
-        } 
-        if (m === 60){
-        
-            m = 00;
-            h = h+1;
-           
+function time(h, m, s) {
+    setInterval(() => {
+
+        if (s < 60) {
+
+            d.innerHTML = `tiempo transcurrido ${h}:${m}:${s}`;
+            s = s + 1;
+
         }
-        if (h === 24)
-        {
-            
+        if (s === 60) {
+            s = 00;
+            m = m + 1;
+        }
+        if (m === 60) {
+
+            m = 00;
+            h = h + 1;
+
+        }
+        if (h === 24) {
+
             h = 00;
         }
 
@@ -35,5 +34,5 @@ setInterval(() => {
     }, 1000);
 
 }
-time (h,m,s);
+time(h, m, s);
 
